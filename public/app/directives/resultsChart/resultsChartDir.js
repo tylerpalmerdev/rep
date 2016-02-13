dee271433051f37960490d7564b45246c982d0bc
Tyler Palmer <mr.tylerpalmer@gmail.com>
2016-02-13 16:28:53 -0700

    color spectrum for results chart. added styling and started to get answer text into chart.

--- public/app/directives/resultsChart/resultsChartDir.js
+++ public/app/directives/resultsChart/resultsChartDir.js
@@ -3,27 +3,66 @@ repApp.directive('resultsChart', function() {
     templateUrl: 'app/directives/resultsChart/resultsChartTmpl.html',
     restrict: 'E',
     scope: {
-      questionData: '='
+      questionData: '=' // [{votes: 70, text: 'blhkh'}]
     },
     replace: false,
     link: function(scope, element, attrs) {
-      var chart = d3.select(element[0]);
+      var chart = d3.select(element[0]),
+          options = scope.questionData.options,
+          totalVotes = 0,
+          highestVal = 0,
+          lowestVal = 0;
 
-      chart.append("div")
+      // get total votes for %s and highest/lowest vote counts
+      options.forEach(function(elem, i, arr) {
+        totalVotes += elem.votes;
+        if (elem.votes > highestVal) {
+          highestVal = elem.votes;
+        }
+
+        if(!lowestVal) {
+          lowestVal = elem.votes;
+        } else if (elem.votes < lowestVal) {
+          lowestVal = elem.votes;
+        }
+      });
+
+      var parDiv = chart.append("div")
       .attr("class", "chart")
       .selectAll("div")
-      .data(scope.questionData.options.sort(function(a, b) {
+      .data(options.sort(function(a, b) {
         return b.votes - a.votes;
       }))
       .enter()
       .append("div")
+      .style("width","100%");
+
+      parDiv.append("div").text(function(d){
+        return d.text;
+      });
+      parDiv.append("div").attr("class", "chart-bar")
       .transition().ease("elastic")
+      .style("background-color", function(d) {
+        var maxRgb = 255,
+            minRgb = 75,
+            valRange = (highestVal - lowestVal),
+            basePct = 1 - ((highestVal - d.votes) / valRange),
+            baseRgb = maxRgb * basePct,
+            colorVal = baseRgb + (1 - basePct) * minRgb;
+
+        return "rgb(0, 0, " + colorVal + ")";
+      })
       .style("width", function (d) {
-        return d.votes + 'px';
+        var pctOfHighest = ((d.votes / highestVal) * 100);
+        return pctOfHighest+ '%';
       })
       .text(function(d) {
-        return d.votes + " votes";
+        var pct = ((d.votes / totalVotes) * 100).toFixed(1);
+        return pct + "% (" + d.votes + " votes)";
       });
+
+
+
     }
   };
 });
